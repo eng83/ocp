79a9e0a3e1d07152f24bd772d49af40628254014
KimYoungKook <youngkook.kim@ksoe.co.kr>
2019-11-15 11:44:27 +0900

    hello

---
+++ src/main/java/com/example/kook/KookApplication.java
@@ -0,0 +1,13 @@
+package com.example.kook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KookApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/kook/kook/web.java
@@ -0,0 +1,17 @@
+package com.example.kook.kook;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+
+public class web {
+    @GetMapping(path = "/{name}")
+    public String hello(@PathVariable String name){
+        return "hello " + name;
+    }
+
+}
